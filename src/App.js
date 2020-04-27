4d2a28ec2fef9b161163d5b30f4ade22d96a804a
balap7 <balasundaram.m@probeseven.com>
2020-04-27 13:42:42 +0530

    content changed from future branch

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React Docker</h2>
         </div>
         <p className="App-intro">
-          I was changed for travis
+          I was changed from feature branch
         </p>
       </div>
     );
