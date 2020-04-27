57b1cde76042c01bf418a8da573deda05288a74b
balap7 <balasundaram.m@probeseven.com>
2020-04-27 12:40:49 +0530

    Updated content

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React Docker</h2>
         </div>
         <p className="App-intro">
-          I was changed
+          I was changed for travis
         </p>
       </div>
     );
